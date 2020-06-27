3c16962a6f45cba394f5d0550e03536727dddd6f
Marjorie Nunes Ribeiro <marjorie.ribeiro@bgmrodotec.com.br>
2020-06-27 20:57:53 -0300

    add new components

--- e-commerce/src/app/app.module.ts
+++ e-commerce/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { HistoricComponent } from './historic/historic.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { DetailsComponent } from './details/details.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HistoricComponent,
+    ShoppingCartComponent,
+    DetailsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
