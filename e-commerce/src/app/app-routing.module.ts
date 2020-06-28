aad455fd9ad39a4f02ddf6eb4877c83959f9b7ed
Marjorie Nunes Ribeiro <marjorie.ribeiro@bgmrodotec.com.br>
2020-06-27 21:24:30 -0300

    create routes

--- e-commerce/src/app/app-routing.module.ts
+++ e-commerce/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HistoricComponent } from './historic/historic.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'detalhes-do-produto', component: DetailsComponent },
+  { path: 'historico-de-compras', component: HistoricComponent },
+  { path: 'carrinho-de-compras', component: ShoppingCartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
