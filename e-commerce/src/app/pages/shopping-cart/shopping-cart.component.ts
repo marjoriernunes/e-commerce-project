5685dd3f90de0bddadacc57bed0c0db83eb0c01a
Marjorie Nunes Ribeiro <marjorie.ribeiro@bgmrodotec.com.br>
2020-06-30 19:29:55 -0300

    update shopping-cart component

--- e-commerce/src/app/pages/shopping-cart/shopping-cart.component.ts
+++ e-commerce/src/app/pages/shopping-cart/shopping-cart.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
 
 export interface PeriodicElement {
   productName: string;
   img: string;
   productPrice: number;
   quantity: number;
+  index: number;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {img: 'assets/images/products/product-08.jpg', productName: 'aparador', productPrice: 1.0079, quantity: 1},
-  {img: 'assets/images/products/product-08.jpg', productName: 'aparador', productPrice: 4.0026, quantity: 2},
-  {img: 'assets/images/products/product-08.jpg', productName: 'aparador', productPrice: 6.941, quantity: 1}
+  {img: 'assets/images/products/product-08.jpg', productName: 'aparador', productPrice: 1.0079, quantity: 1, index: 0},
+  {img: 'assets/images/products/product-08.jpg', productName: 'aparador', productPrice: 4.0026, quantity: 2, index: 1},
+  {img: 'assets/images/products/product-08.jpg', productName: 'aparador', productPrice: 6.941, quantity: 1, index: 2}
 ];
 
 @Component({
@@ -20,7 +22,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 
 export class ShoppingCartComponent implements OnInit {
-  displayedColumns: string[] = ['img', 'productName', 'productPrice', 'quantity'];
+  displayedColumns: string[] = ['img', 'productName', 'productPrice', 'quantity', 'deleteProduct'];
   dataSource = ELEMENT_DATA;
 
   cartQuantity: number;
@@ -30,6 +32,14 @@ export class ShoppingCartComponent implements OnInit {
   ngOnInit(): void {
     this.cartQuantity = this.dataSource.length;
     localStorage.setItem('cartQuantity', this.cartQuantity.toString());
+  }
+
+  removeItem(index) {
+    let array = [0, 1, 2];
+    let events = _.remove(array, function(n) {
+      return n === index;
+    });
+    console.log(events);
   }
 
   confirmPurchase() {}
